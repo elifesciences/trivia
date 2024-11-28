2d6d273f6d096cab0721202f14c484a4609be959
Daniel Haarhoff <daniel@rknt.de>
2024-11-28 11:20:50 +0000

    Remove unnecessary initialisation of a variable

--- php/GameRunner.php
+++ php/GameRunner.php
@@ -2,21 +2,19 @@
 
 include __DIR__.'/Game.php';
 
-$notAWinner;
+$aGame = new Game();
 
-  $aGame = new Game();
-
-  $aGame->add("Chet");
-  $aGame->add("Pat");
-  $aGame->add("Sue");
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
 
 
-  do {
-      $aGame->roll(rand(0, 5) + 1);
+do {
+  $aGame->roll(rand(0, 5) + 1);
 
-      if (rand(0, 9) == 7) {
-          $notAWinner = $aGame->wrongAnswer();
-      } else {
-          $notAWinner = $aGame->wasCorrectlyAnswered();
-      }
-  } while ($notAWinner);
+  if (rand(0, 9) == 7) {
+      $notAWinner = $aGame->wrongAnswer();
+  } else {
+      $notAWinner = $aGame->wasCorrectlyAnswered();
+  }
+} while ($notAWinner);
