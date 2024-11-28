5be1a1114760a8f3ae384a3800737111ca06d383
LinaKind <l.kinduryte@elifesciences.org>
2024-11-28 11:23:29 +0000

    Name an expression

--- php/GameRunner.php
+++ php/GameRunner.php
@@ -8,9 +8,9 @@ $aGame->add("Chet");
 $aGame->add("Pat");
 $aGame->add("Sue");
 
-
 do {
-  $aGame->roll(rand(0, 5) + 1);
+  $rollOfTheDice = rand(0, 5) + 1;
+  $aGame->roll($rollOfTheDice);
 
   if (rand(0, 9) == 7) {
       $notAWinner = $aGame->wrongAnswer();
