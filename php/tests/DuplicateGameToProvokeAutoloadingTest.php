287ee558b1e5587c05876282e516cc1e8851bbf3
David Moulton <d.moulton@elifesciences.org>
2025-02-18 13:23:49 +0000

    Provoke a pull to have more reusable auto load when testing

---
+++ php/tests/DuplicateGameToProvokeAutoloadingTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class DuplicateGameToProvokeAutoloadingTest extends TestCase
+{
+    /**
+     * @dataProvider addingPlayersProvider
+     */
+    public function testAddingPlayers(array $players, int $expectedPlayerCount): void
+    {
+        $game = new Game();
+        foreach ($players as $player) {
+            $game->add($player);
+        }
+        $this->assertSame($expectedPlayerCount, $game->howManyPlayers());
+    }
+
+    public function addingPlayersProvider(): array
+    {
+        return [
+            [
+                [], // no players
+                0   // expected player count
+            ],
+            [
+                ['Alice'], // 1 player
+                1   // expected player count
+            ],
+        ];
+    }
+}
